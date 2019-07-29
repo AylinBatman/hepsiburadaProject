ce7d216dee1091accbb740559977f6d7d5bc85dd
Aylin Batman <namtabaylin@hotmail.com>
2019-07-29 14:00:47 +0300

    General_Contants

---
+++ src/main/java/com/aut/pages/General_Contants.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.By;
+
+public interface General_Contants {
+
+    By EMAIL_TEXTBOX = By.id("email");
+    By PASSWORD_TEXTBOX = By.id("password");
+    By FIRSTNAME_TEXTBOX = By.id("firstname");
+    By LASTNAME_TEXTBOX = By.id("email-register");
+    By EMAIL_REGISTER_TEXTBOX = By.id("email-register");
+    By PASSWORD_REGISTER_TEXTBOX = By.id("password-register");
+    By PASSWORD_REPEAT_TEXTBOX = By.id("password-repeat");
+}
